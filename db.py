ddab75a2c704d3acfd73a1a459f2f731a3b36607
kaustav <2004.k.c2@gmail.com>
2025-06-11 04:29:16 +0530

    WEB-SERVER

--- db.py
+++ db.py
@@ -1,10 +1,10 @@
 import psycopg2
 
 def get_connection():
-    return mysql.connector.connect(
+    return psycopg2.connect(
         host="dpg-d14akindiees73d3uvl0-a",
         user="lms_uxw7_user",
         password="yQwHBL2HHRoTtAZPc443HzHLeLwfUi0x",
-        database="lms_uxw7",
+        dbname="lms_uxw7",
         port=5432
     )
