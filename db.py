7454827f2ce8efdf1922f2ca713a2cf7f0726e92
kaustav <2004.k.c2@gmail.com>
2025-06-11 04:40:58 +0530

    ADDING WEB-SERVER

--- db.py
+++ db.py
@@ -1,10 +1,11 @@
 import psycopg2
+import os
 
 def get_connection():
     return psycopg2.connect(
-        host="dpg-d14akindiees73d3uvl0-a",
-        user="lms_uxw7_user",
-        password="yQwHBL2HHRoTtAZPc443HzHLeLwfUi0x",
-        dbname="lms_uxw7",
-        port=5432
+        host=os.getenv("DB_HOST", "dpg-d14akindiees73d3uvl0-a"),
+        user=os.getenv("DB_USER", "lms_uxw7_user"),
+        password=os.getenv("DB_PASSWORD", "yQwHBL2HHRoTtAZPc443HzHLeLwfUi0x"),
+        dbname=os.getenv("DB_NAME", "lms_uxw7"),
+        port=os.getenv("DB_PORT", 5432)
     )
